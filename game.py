11c914e1537cfd983ad33723aa4423b1c8e1f087
Lars Wettstein <lars@pop-os.localdomain>
2020-11-12 12:06:32 +0100

    commit

---
+++ game.py
@@ -0,0 +1,31 @@
+players = []
+winner = False 
+temp = 0
+roundCount = 0
+
+def getActivePlayer(roundCount, players):
+    return players[roundCount%len(players)]
+
+def registerPlayers(players):
+    numberOfPlayers = input("With how many  you want to play? ")    
+    for i in range(int(numberOfPlayers)): 
+        players.append(input('Enter name for player {} '.format(i+1)))
+    return players
+    
+players = registerPlayers(players)
+
+matchesCount = input("With how many matches do you want to play? ")
+
+while winner == False:
+
+    temp = input("{}: how many matcher do you want to take? (pick between 1 and 3) ".format(getActivePlayer(roundCount, players)))
+
+    matchesCount = int(matchesCount) - int(temp)
+    print("Rest: {}".format(matchesCount))
+    temp = 0
+   
+    if matchesCount == 0:    
+       print("{} is the WINNER!!!".format(getActivePlayer(roundCount, players)))
+       winner = True
+
+    roundCount += 1
